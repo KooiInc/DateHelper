70edeb6962a9334026853bc8971db4ed42ba50a7
Renzo Kooi <renzo.kooi@gmail.com>
2018-06-02 08:39:46 +0200

    Renames/impl

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 module.exports.XDate = ( () => {
-  const { dateSet, parts, setLanguage, format, dateAdd, setValidDate, moduleData, objMerge, partsHelper } = require("./DateHelpers");
+  const { dateSet, dateUnits, setLanguage, format, dateAdd, setValidDate, moduleData, objMerge, units } = require("./DateHelpers");
   
   const Create = dateObj => objMerge(dateObj, {
       add: (val, datePart) => dateAdd(dateObj, datePart, val),
       setPart: (val, datePart) => dateSet(dateObj, datePart, val),
       setLanguage: language => setLanguage(dateObj, language),
       format: (formatStr, language) => format(dateObj, formatStr, language),
-      parts: partsHelper,
+      units: units,
     });
 
   return (someDate, language = moduleData.defaultLanguage) =>
