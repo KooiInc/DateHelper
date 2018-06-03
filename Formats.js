c49d87ba8d09c8db36e01eb97f65749383076e5b
Renzo Kooi <renzo.kooi@gmail.com>
2018-06-03 18:08:08 +0200

    Order and comment

--- Formats.js
+++ Formats.js
@@ -1,20 +1,23 @@
 module.exports = {
+    // ISO
     dateISO: (separator = "-") => `yyyy${separator}mm${separator}dd`,
-    dateMonthFullEN: "MM d yyyy",
-    dateMonthFull: "d MM yyyy",
-    dateMonthWeekDayFullEN: "DOW MM d yyyy",
-    dateMonthWeekDayFull: "DOW d MM yyyy",
-    dateMonthFullWeekDayShortEN: "dow MM d yyyy",
-    dateMonthFullWeekDayShort: "dow d MM yyyy",
     dateTimeISOFull: (separator = "-") => `yyyy${separator}mm${separator}dd hh:MI:S.MS`,
     dateTimeISOSeconds: (separator = "-") => `yyyy${separator}mm${separator}dd hh:MI:S`,
     dateTimeISO: (separator = "-") => `yyyy${separator}mm${separator}dd hh:MI`,
+    // EN
+    dateMonthFullEN: "MM d yyyy",
+    dateMonthWeekDayFullEN: "DOW MM d yyyy",
+    dateMonthFullWeekDayShortEN: "dow MM d yyyy",
     dateTimeMonthFullWeekDayShortEN: "dow MM d yyyy hh:MI",
-    dateTimeMonthFullWeekDayShort: "dow d MM yyyy hh:MI",
     dateTimeMonthFullEN: "MM d yyyy hh:MI",
-    dateTimeMonthFull: "d MM yyyy hh:MI",
     dateTimeMonthWeekDayFullEN: "DOW MM d yyyy hh:MI",
-    dateTimeMonthShortWeekDayFull: "DOW dd M d yyyy hh:MI",
     dateTimeMonthWeekDayShortEN: "dow M d yyyy hh:MI",
+    // Rest of the world
+    dateMonthFull: "d MM yyyy",
+    dateMonthWeekDayFull: "DOW d MM yyyy",
+    dateMonthFullWeekDayShort: "dow d MM yyyy",
+    dateTimeMonthFullWeekDayShort: "dow d MM yyyy hh:MI",
+    dateTimeMonthFull: "d MM yyyy hh:MI",
+    dateTimeMonthShortWeekDayFull: "DOW dd M d yyyy hh:MI",
     dateTimeMonthShortWeekDayShort: "dow d M yyyy hh:MI",
 };
