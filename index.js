8a79aa448d9facd759efb9861eb5774ef2816bd6
Renzo Kooi <renzo.kooi@gmail.com>
2018-06-06 15:36:10 +0200

    Rename, cleanup and refactor (no need for inline methods)

--- index.js
+++ index.js
@@ -1,32 +1,28 @@
+const {
+  dateAdd,
+  dateSet,
+  setLanguage,
+  format,
+  units,  
+  setValidDate,
+  moduleData,
+} = require("./DateHelpers");
+
+function Instance(date, language) {
+  this.value = date;
+  this.language = language;
+} 
+
+Instance.prototype = {
+  add(val, unit = units.day) { return dateAdd(this, unit, val); },
+  setUnit(val, unit) { return dateSet(this, unit, val); },
+  setLanguage(language) { return setLanguage(this, language); },
+  format(formatStr, language) {return format(this, formatStr, language); },
+  units: units,
+};
+
 module.exports = {
-  XDate: (() => {
-    const {
-      dateSet,
-      dateUnits,
-      setLanguage,
-      format,
-      dateAdd,
-      setValidDate,
-      moduleData,
-      objMerge,
-      units
-    } = require("./DateHelpers");
-
-    function XDateCtor(date, language) {
-      this.value = date;
-      this.language = language;
-    } 
-
-    XDateCtor.prototype = {
-      add(val, unit = units.day) { return dateAdd(this, unit, val); },
-      setUnit(val, unit) { return dateSet(this, unit, val); },
-      setLanguage(language) { return setLanguage(this, language); },
-      format(formatStr, language) {return format(this, formatStr, language); },
-      units: units,
-    };
-
-    return (someDate, language = moduleData.defaultLanguage) =>
-      new XDateCtor(setValidDate(someDate ? new Date(someDate) : new Date()), language);
-  })(),
   formatStrings: require("./Formats"),
+  XDate: (someDate, language = moduleData.defaultLanguage) =>
+      new Instance(setValidDate(someDate ? new Date(someDate) : new Date()), language),
 };
